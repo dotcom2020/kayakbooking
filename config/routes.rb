4990d68ad59e9df14641109ef464c01c7e60e96d
nicolas nguon <nicolas.nguon@checkmyguest.fr>
2022-08-25 20:05:11 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :kayaks
+
+  get '/my_bookings', to: 'dashboards#my_bookings'
+  get '/booking_received', to: 'dashboard#bookings_received'
+
 end
